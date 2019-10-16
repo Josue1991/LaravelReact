9883a9ee2b48df8e5a6d549f0f082e4a230d117d
Josue1991 <josca0221@gmail.com>
2019-10-16 01:08:16 -0500

    cambios finales

--- LaravelReact/routes/api.php
+++ LaravelReact/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Producto;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,32 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('productos', function () {
+    return response(Product::all(),200);
+});
+ 
+Route::get('productos/{producto}', function ($productId) {
+    return response(Product::find($productId), 200);
+});
+  
+ 
+Route::post('productos', function(Request $request) {
+   $resp = Product::create($request->all());
+    return $resp;
+ 
+});
+ 
+Route::put('productos/{producto}', function(Request $request, $productId) {
+    $product = Product::findOrFail($productId);
+    $product->update($request->all());
+    return $product;
+});
+ 
+Route::delete('productos/{producto}',function($productId) {
+    Product::find($productId)->delete();
+ 
+    return 204;
+ 
 });
